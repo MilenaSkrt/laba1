c0fa6c17241bc6c6cd0fa0865c843105c3911bc2
MilenaSkrt <135116799+MilenaSkrt@users.noreply.github.com>
2023-11-13 18:15:44 +0500

    Update 2.c

--- uuu/2.c
+++ uuu/2.c
@@ -12,3 +12,25 @@ int main() {
     printf("x = %lf\n", x);
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    double a, b, c, x;
+    printf("Write a: ");
+    scanf("%lf", &a);
+    printf("Write b: ");
+    scanf("%lf", &b);
+    printf("Write c: ");
+    scanf("%lf", &c);
+
+    if (a != 0) {
+        x = (c - b) / a;
+        printf("x = %lf\n", x);
+    } else {
+        printf("Division by zero\n");
+    }
+    
+    return 0;
+}
